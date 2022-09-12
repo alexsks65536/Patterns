f95d42732b1a75692a6bed4f5ddd96db51d2ef43
alexsks65536 <megbyte@mail.ru>
2022-09-12 22:36:54 +0300

    Урок 3. исправление ошибок

--- Lesson_1_Sekisov/simba_framework/templator.py
+++ Lesson_1_Sekisov/simba_framework/templator.py
@@ -10,7 +10,10 @@ def render(template_name, folder='templates', **kwargs):
     :return:
     """
 
+    # создаем объект окружения
     env = Environment()
+    # указываем папку для поиска шаблонов
     env.loader = FileSystemLoader(folder)
+    # находим шаблон в окружении
     template = env.get_template(template_name)
     return template.render(**kwargs)
